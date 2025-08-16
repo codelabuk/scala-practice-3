564f08142b7a7be2124067bde8106bc38ee91650
codelabuk <afzal232@gmail.com>
2025-08-16 18:36:06 +0530

    Add boolean & string maniuplation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,22 @@
 @main def hello: Unit = {
   println("Hello World")
   println(msg)
-
-  val abc: String = "x"
+  println(sum(5,2))
+  println(switchOn(switchOn = true))
+  println(switchOn(switchOn = false))
+  println(switchOn())
+  println(product(2,4))
+  println(greeting(name = "value"))
+  print(greeting())
 }
 
-def msg: String = return "I was compiled by Scala 3"
+def msg: String = "I was compiled by Scala 3"
+
+def sum(n1: Int, n2: Int) = n1 + n2
+
+def switchOn(switchOn: Boolean = false): String = if switchOn then "light On!" else "Light off"
+
+def product(n1: Int, n2: Int): Int = n1*n2
+
+def greeting(name: String = "key"): String =  s"hi $name"
 
