6548c4d87ec0e666b4a94dc9f5390ddd2e53ce74
codelabuk <afzal232@gmail.com>
2025-08-17 19:34:27 +0530

    refactor

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,13 +1,13 @@
 @main def chapter3: Unit = {
   val anne = Person("Anne", 25)
-  println(greeting(anne))
+  println(greeting1(anne))
 
   val anne1 = anne.copy(age = 36)
-  println(greeting(anne1))
+  println(greeting1(anne1))
 }
 
 
-def greeting(person: Person) : String = s"hi ${person.name} with ${person.age}"
+def greeting1(person: Person) : String = s"hi ${person.name} with ${person.age}"
 
 case class Person(name: String, age: Int)
 
--- src/main/scala/Weather.scala
+++ src/main/scala/Weather.scala
@@ -2,9 +2,9 @@
   println("Hello")
   println(weather(true))
   println(weather(false))
-  println(greeting("Ada"))
-  println(greeting("Adam"))
-  println(greeting1("Adam"))
+  println(greeting2("Ada"))
+  println(greeting2("Adam"))
+  println(greeting3("Adam"))
   println(competition(3))
   println(competition(8))
 
@@ -13,14 +13,14 @@ def weather(sunny: Boolean): String = sunny match {
    case false => " where is my umbrella"
  }
 
-def greeting(freind: String) :String = freind match {
+def greeting2(freind: String) :String = freind match {
   case "Ada" => "Hi Ada"
   case "Deji" => "Hi Deji"
   case _ => "Hi what's ur name"
 }
 
 
-def greeting1(friend: String) :String = friend match {
+def greeting3(friend: String) :String = friend match {
   case "Ada" | "Deji"  => "Hi Friends , Long Time"
   case newFriend => s"Hi $newFriend"
 }
